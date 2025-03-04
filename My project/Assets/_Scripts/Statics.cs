cdbab1ff7a0414fd29aa15986da2168a3e14c15c
badarang <badarangdev@gmail.com>
2025-03-05 08:29:52 +0900

    Add Animation Controller

--- My project/Assets/_Scripts/Statics.cs
+++ My project/Assets/_Scripts/Statics.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public static class Statics
 {
-
+    public static List<int> UnitPickUpChance = new List<int> { 80, 15, 5 };
+    public static List<int> GamblingChance = new List<int> { 50, 30, 15, 5 };
 }
 
 public enum Grade
